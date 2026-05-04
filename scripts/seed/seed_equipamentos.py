4b391fd4fa8a24e5952f14a6af412e5de30a268c
Bruno Garcia <brgarcia@outlook.com>
2026-05-04 12:31:28 -0300

    chore: ajustes finos em seeds e documentação de dashboard

--- scripts/seed/seed_equipamentos.py
+++ scripts/seed/seed_equipamentos.py
@@ -9,26 +9,26 @@ from app.modules.equipamentos.models import ModeloEquipamento, SlotInventario
 
 EQUIPAMENTOS_FICHA = [
     # 1P - POSTO DIANTEIRO
-    {"posicao": "AMPMIC 1P", "nome": "AMPLIFICADOR MIC 1P", "pn": "263-000", "sistema": "1P"},
+    {"posicao": "AMPMIC", "nome": "AMPMIC", "pn": "263-000", "sistema": "1P"},
     {"posicao": "PDU", "nome": "PDU", "pn": "4455-1000-01", "sistema": "1P"},
     {"posicao": "UFCP", "nome": "UFCP", "pn": "4456-1000-02", "sistema": "1P"},
     {"posicao": "CHVC", "nome": "CHVC", "pn": "VEC00054", "sistema": "1P"},
-    {"posicao": "CMFD1", "nome": "COLOR MULTI-FUNCTION DISPLAY", "pn": "MB387B-01", "sistema": "1P"},
-    {"posicao": "CMFD2", "nome": "COLOR MULTI-FUNCTION DISPLAY", "pn": "MB387B-01", "sistema": "1P"},
-    {"posicao": "ASP 1P", "nome": "ASP 1P", "pn": "343-001", "sistema": "1P"},
+    {"posicao": "CMFD", "nome": "COLOR MULTI-FUNCTION DISPLAY", "pn": "MB387B-01", "sistema": "1P"},
+    {"posicao": "CMFD", "nome": "COLOR MULTI-FUNCTION DISPLAY", "pn": "MB387B-01", "sistema": "1P"},
+    {"posicao": "ASP", "nome": "ASP", "pn": "343-001", "sistema": "1P"},
     {"posicao": "GPS", "nome": "GPS RECEIVER", "pn": "066-04031-1622", "sistema": "1P"},
     {"posicao": "PA CONTROL", "nome": "PA CONTROL", "pn": "449300-02-01", "sistema": "1P"},
     {"posicao": "PIC/NAV", "nome": "PAINEL PIC/NAV", "pn": "314-04895-403", "sistema": "1P"},
-    {"posicao": "PUNHO DO MANCHE 1P", "nome": "PUNHO DO MANCHE 1P", "pn": "733-0402", "sistema": "1P"},
+    {"posicao": "PUNHO DO MANCHE", "nome": "PUNHO DO MANCHE", "pn": "733-0402", "sistema": "1P"},
     {"posicao": "DVR", "nome": "DVR", "pn": "MB211E-03", "sistema": "1P"},
 
     # 2P - POSTO TRASEIRO
-    {"posicao": "AMPMIC 2P", "nome": "AMPLIFICADOR MIC 2P", "pn": "263-000", "sistema": "2P"},
+    {"posicao": "AMPMIC", "nome": "AMPMIC", "pn": "263-000", "sistema": "2P"},
     {"posicao": "PSU", "nome": "PSU", "pn": "4458-1000-00", "sistema": "2P"},
-    {"posicao": "CMFD3", "nome": "COLOR MULTI-FUNCTION DISPLAY", "pn": "MB387B-01", "sistema": "2P"},
-    {"posicao": "CMFD4", "nome": "COLOR MULTI-FUNCTION DISPLAY", "pn": "MB387B-01", "sistema": "2P"},
-    {"posicao": "ASP 2P", "nome": "ASP 2P", "pn": "343-001", "sistema": "2P"},
-    {"posicao": "PUNHO DO MANCHE 2P", "nome": "PUNHO DO MANCHE 2P", "pn": "733-0402", "sistema": "2P"},
+    {"posicao": "CMFD", "nome": "COLOR MULTI-FUNCTION DISPLAY", "pn": "MB387B-01", "sistema": "2P"},
+    {"posicao": "CMFD", "nome": "COLOR MULTI-FUNCTION DISPLAY", "pn": "MB387B-01", "sistema": "2P"},
+    {"posicao": "ASP", "nome": "ASP", "pn": "343-001", "sistema": "2P"},
+    {"posicao": "PUNHO DO MANCHE", "nome": "PUNHO DO MANCHE", "pn": "733-0402", "sistema": "2P"},
 
     # CEI - COMPARTIMENTO ELETRÔNICO INFERIOR
     {"posicao": "ADF", "nome": "ADF RECEIVER", "pn": "622-7382-101", "sistema": "CEI"},
@@ -41,8 +41,8 @@ EQUIPAMENTOS_FICHA = [
     {"posicao": "MDP2", "nome": "MULTI-DISPLAY PROCESSOR", "pn": "MA902B-02", "sistema": "CEI"},
     {"posicao": "ARTU", "nome": "ARTU", "pn": "251-118-012-012", "sistema": "CEI"},
     {"posicao": "AFDC", "nome": "AFDC", "pn": "449100-02-01", "sistema": "CEI"},
-    {"posicao": "VUHF1", "nome": "VUHF RADIO", "pn": "6110.3001.12", "sistema": "CEI"},
-    {"posicao": "VUHF2", "nome": "VUHF RADIO", "pn": "6106.7006.12", "sistema": "CEI"},
+    {"posicao": "VUHF1", "nome": "VUHF-1", "pn": "6110.3001.12", "sistema": "CEI"},
+    {"posicao": "VUHF2", "nome": "VUHF-2", "pn": "6106.7006.12", "sistema": "CEI"},
 
     # CES - COMPARTIMENTO ELETRÔNICO SUPERIOR
     {"posicao": "VADR", "nome": "VADR", "pn": "174521-10-01", "sistema": "CES"},
